3b1a4b891e979d43529596b66b23bcd742ba62fa
Ian King <kickthechair@gmail.com>
2020-05-14 10:33:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  Array = ["first", "second", "third", "forth"]
+  array = ["first", "second", "third", "forth"]
 end
 
 def add_element_to_end_of_array(array, element)
